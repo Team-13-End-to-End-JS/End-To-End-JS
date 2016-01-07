ee6077afd23799a243b63bcfa9120ae5e574c348
g-yonchev <georgi.d.yonchev@gmail.com>
2016-01-07 03:39:15 +0200

    Added some properties and validations to RealEstate model

--- server/data/models/RealEstate.js
+++ server/data/models/RealEstate.js
@@ -6,9 +6,36 @@
 
     module.exports.init = function() {
         let realEstateSchema = new Schema({
-            title: String,
-            description: String,
-            price: Number
+            title: {
+                type: String,
+                validate: function (input) {
+                    return (input.length <= 0 && input.length >= 50);
+                },
+                require: '{PATH} is required',
+                message: '{PATH} title should be between 1 and 50 symbols'
+            },
+            description: {
+                type: String,
+                validate: function (input) {
+                    return (input.length <= 0 && input.length >= 500);
+                },
+                require: '{PATH} is required',
+                message: '{PATH} description should be between 1 and 500 symbols'
+            },
+            rentPrice: {
+                type: Number,
+                validate: function (input) {
+                    return input <= 0;
+                },
+                message: '{PATH} renting price should be a positive number'
+            },
+            sellPrice: {
+                type: Number,
+                validate: function (input) {
+                    return input <= 0;
+                },
+                message: '{PATH} selling price should be a positive number'
+            }
         });
 
         let RealEstate = mongoose.model('RealEstate', realEstateSchema);
