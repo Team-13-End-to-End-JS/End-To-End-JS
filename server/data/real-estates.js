aa9fd96cfb26938feb9221f0005d83bab343b664
g-yonchev <georgi.d.yonchev@gmail.com>
2016-01-09 23:03:56 +0200

    New real estate now is added in the database

--- server/data/real-estates.js
+++ server/data/real-estates.js
@@ -3,24 +3,35 @@
 
     let RealEstate = require('mongoose').model('RealEstate');
     let User = require('mongoose').model('User');
+    let Location = require('mongoose').model('Location');
 
     function create(realEstate) {
+        let locationName = realEstate.location;
         let newRealEstate = {
             title: realEstate.title,
             description: realEstate.description,
-            sellPrice: realEstate.sellPrice,
             quadrature: realEstate.quadrature,
             year: realEstate.year
         };
 
-        let promise = new Promise(function (resolve, reject) {
-            RealEstate.create(newRealEstate, function (err, createdRealEstate) {
+        let locationDb;
+        Location.findOne({name: locationName}).exec(function (err, location) {
+            if (err) console.log(err);
+            else {
+                locationDb = location;
+            }
+        });
+
+        let promise = new Promise(function(resolve, reject) {
+            RealEstate.create(newRealEstate, function(err, createdRealEstate) {
                 if (err) {
                     return reject(err);
                 }
 
+                locationDb.realEstates.push(createdRealEstate);
+                console.log(locationDb);
                 resolve(createdRealEstate);
-            });
+            }) ;
         });
 
         return promise;
