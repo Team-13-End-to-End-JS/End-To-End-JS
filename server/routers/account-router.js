a1e3774f3a2ac95b5d125bac7942243fa7ff2229
Pip3r4o <pip3r4o@gmail.com>
2016-01-11 21:30:44 +0200

    Unicorns and all that is sacred and magical. PEACE (Y) Actually, user profile preparations....

--- server/routers/account-router.js
+++ server/routers/account-router.js
@@ -10,7 +10,10 @@
         .post('/register', controllers.users.register)
         .get('/login', controllers.users.getLogin)
         .post('/login', controllers.users.login)
-        .get('/logout', auth.isAuthenticated, controllers.users.logout);
+        .get('/logout', auth.isAuthenticated, controllers.users.logout)
+        .get('/profile', controllers.users.getCurrentUserProfile)
+        .get('/profile/:username', controllers.users.getUserProfile);
+
 
     module.exports = function(app) {
         app.use('/', router);
