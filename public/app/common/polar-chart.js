adaeff0edf08b521aa131102eabb552d4521c992
g-yonchev <georgi.d.yonchev@gmail.com>
2016-01-12 22:59:36 +0200

    Statistics for construction type and for real estate type.

--- public/app/common/polar-chart.js
+++ public/app/common/polar-chart.js
@@ -6,25 +6,79 @@
         })
         .then(function (statistics) {
 
-            var data = [
-                {
-                    value: statistics.stats.forRent,
-                    color: "#F7464A",
-                    highlight: "#FF5A5E",
-                    label: "For Rent"
-                },
-                {
-                    value: statistics.stats.forSale,
-                    color: "#46BFBD",
-                    highlight: "#5AD3D1",
-                    label: "For Sale"
-                },
+            let colors = [
+                "#99cc33", "#F7464A", "#46BFBD", "#FDB45C", "#9966ff", "#888BDC", "#DEF25C"
             ];
 
-            if (document.getElementById("chart-area")) {
+            let highlights = [
+                "#99cc66", "#FF5A5E", "#5AD3D1", "#FFC870", "#9933cc", "#9FA1E3", "#BED435"
+            ];
 
-                var ctx = document.getElementById("chart-area").getContext("2d");
-                window.myPolarArea = new Chart(ctx).Pie(data, {
+            let offerTypes = [];
+
+            for (let property in statistics.offerTypes) {
+                if (statistics.offerTypes.hasOwnProperty(property)) {
+                    let random = Math.floor(Math.random() * colors.length);
+                    offerTypes.push({
+                        value: statistics.offerTypes[property],
+                        color: colors[random],
+                        highlight: highlights[random],
+                        label: property
+                    });
+                }
+            }
+
+
+            let realEstateTypes = [];
+
+            for (let property in statistics.realEstateTypes) {
+                if (statistics.realEstateTypes.hasOwnProperty(property)) {
+                    let random = Math.floor(Math.random() * colors.length);
+                    realEstateTypes.push({
+                        value: statistics.realEstateTypes[property],
+                        color: colors[random],
+                        highlight: highlights[random],
+                        label: property
+                    });
+                }
+            }
+
+            let constructionTypes = [];
+
+            for (let property in statistics.constructionTypes) {
+                if (statistics.constructionTypes.hasOwnProperty(property)) {
+                    let random = Math.floor(Math.random() * colors.length);
+                    constructionTypes.push({
+                        value: statistics.constructionTypes[property],
+                        color: colors[random],
+                        highlight: highlights[random],
+                        label: property
+                    });
+                }
+            }
+
+            if (document.getElementById("chart-area-offer-types")) {
+
+                let ctx = document.getElementById("chart-area-offer-types").getContext("2d");
+                window.myPolarArea = new Chart(ctx).Pie(offerTypes, {
+                    responsive: true
+                });
+
+            }
+
+            if (document.getElementById("chart-area-real-estate-types")) {
+
+                let ctx = document.getElementById("chart-area-real-estate-types").getContext("2d");
+                window.myPolarArea = new Chart(ctx).Pie(realEstateTypes, {
+                    responsive: true
+                });
+
+            }
+
+            if (document.getElementById("chart-area-construction-types")) {
+
+                let ctx = document.getElementById("chart-area-construction-types").getContext("2d");
+                window.myPolarArea = new Chart(ctx).Pie(constructionTypes, {
                     responsive: true
                 });
 
--- server/controllers/statistics-controller.js
+++ server/controllers/statistics-controller.js
@@ -2,27 +2,75 @@
     'use strict';
 
     let RealEstate = require('mongoose').model('RealEstate'),
-        stats = {};
+        RealEstateType = require('mongoose').model('RealEstateType'),
+        ConstructionType = require('mongoose').model('ConstructionType'),
+        stats = {
+            offerTypes: {
+                forSale: 0,
+                forRent: 0
+            },
+            realEstateTypes: {},
+            constructionTypes: {}
+        },
+        realEstateTypes,
+        constructionTypes,
+        helper = false;
 
 
     function getStats(req, res) {
-        RealEstate.find({offerType: 'forRent'})
-            .exec(function (err, realEstates) {
-                if(err) { console.log(err); }
 
-                stats.forRent = realEstates.length;
+        RealEstateType.find({}).exec(function (err, collection) {
+            if(err) {
+                console.log(err);
+            }
 
-                RealEstate.find({offerType: 'forSale'})
-                    .exec(function (err, realEstates) {
-                        if(err) { console.log(err); }
+            realEstateTypes = collection[0].types;
 
-                        stats.forSale = realEstates.length;
+            ConstructionType.find({}).exec(function (err, collection) {
+                if(err) {
+                    console.log(err);
+                }
 
-                        res.send({stats: stats});
-                        res.end();
-                    });
+                constructionTypes = collection[0].types;
 
+                RealEstate.find({}).exec(function (err, realEstates) {
+                    if(err) {
+                        console.log(err);
+                    }
+
+                    if (!helper) {
+                        for (var i = 0; i < realEstates.length; i++) {
+                            var realEstate = realEstates[i];
+
+                            if(realEstate.offerType === 'forSale') {
+                                stats.offerTypes.forSale++;
+                            }
+
+                            if(realEstate.offerType === 'forRent') {
+                                stats.offerTypes.forRent++;
+                            }
+
+                            if(stats.realEstateTypes[realEstate.realEstateType] === undefined) {
+                                stats.realEstateTypes[realEstate.realEstateType] = 1;
+                            } else {
+                                stats.realEstateTypes[realEstate.realEstateType]++;
+                            }
+
+                            if(stats.constructionTypes[realEstate.constructionType] === undefined) {
+                                stats.constructionTypes[realEstate.constructionType] = 1;
+                            } else {
+                                stats.constructionTypes[realEstate.constructionType]++;
+                            }
+                        }
+
+                        helper = true;
+                    }
+
+                    res.send(stats);
+                    res.end();
+                });
             });
+        });
     }
 
     module.exports = {
